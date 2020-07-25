53d0fbd2bc43dea96405a5a444b279377f7fa8a1
Fernando Reis Guimarães <fernandobhz@gmail.com>
2020-07-25 19:04:47 -0300

    old project migrated

---
+++ ExitCode/Module1.vb
@@ -0,0 +1,23 @@
+﻿Module Module1
+
+    Sub Main()
+
+        Dim Args() As String = System.Environment.GetCommandLineArgs()
+
+        Dim ec As Integer
+        If Args.Count = 2 Then
+            Try
+                ec = Args(1)
+            Catch ex As Exception
+                ec = -1
+            End Try
+        Else
+            ec = -2
+        End If
+
+        MsgBox("vou sair")
+        Environment.Exit(Args(1))
+        MsgBox("sai")
+    End Sub
+
+End Module
---
+++ Get Exit Code/Form1.vb
@@ -0,0 +1,19 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles Button1.Click
+        Dim startInfo As New System.Diagnostics.ProcessStartInfo
+        Dim MyProces As Process
+
+        startInfo.FileName = "..\..\..\ExitCode\bin\Debug\ExitCode.exe"
+        startInfo.Arguments = InputBox("Enter exit code to send and verify if that code returns")
+        startInfo.CreateNoWindow = True
+        startInfo.UseShellExecute = True
+        startInfo.WindowStyle = ProcessWindowStyle.Hidden
+
+        MyProces = Process.Start(startInfo)
+        MyProces.WaitForExit()
+
+        MsgBox(MyProces.ExitCode)
+        MsgBox("0x" + Hex(MyProces.ExitCode()))
+    End Sub
+End Class
